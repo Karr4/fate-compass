6d9a89521218f6bb6e3d83047cae2a7905b01a8f
supreme-cake-being <XGon20987@gmail.com>
2023-05-11 21:30:38 +0300

    added mod.js

---
+++ js/mod.js
@@ -0,0 +1,12 @@
+export default function mod(number) {
+    if (number > 22) {
+        let newNumber = 0;
+        number = number.toString().split("");
+
+        for (let num of number) 
+            newNumber += parseInt(num);
+        
+        return mod(newNumber);
+    }
+    return number;
+}
