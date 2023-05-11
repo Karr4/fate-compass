ca31ae8c69623dabe81e2f8acbd711a7a7912db5
supreme-cake-being <XGon20987@gmail.com>
2023-05-11 21:30:57 +0300

    created array of cards

---
+++ js/cards.js
@@ -0,0 +1,22 @@
+export const cards = [
+    {
+        name: "shit",
+        picture: "shit",
+        desdcription: "shit",
+    },
+    {
+        name: "bitch",
+        picture: "bitch",
+        desdcription: "bitch",
+    },
+    {
+        name: "fuck",
+        picture: "fuck",
+        desdcription: "fuck",
+    },
+    {
+        name: "hodov",
+        picture: "hodov",
+        desdcription: "hodov",
+    },
+];
