8fb748ebf74a4a1f9fd7addd3b28a26f6cb99928
supreme-cake-being <XGon20987@gmail.com>
2023-05-12 12:27:55 +0300

    improved matrix.js

--- js/matrix.js
+++ js/matrix.js
@@ -1,69 +1,96 @@
 import mod from "./mod.js";
 
 const matrix = document.querySelector("ul.matrix");
-// const birthday = document.querySelector(".birthday");
-// console.log(birthday.value);
-    
-let day = 5;
-let month = 10;
-let year = 2003;
-const matrixData = [];
+const matrixForm = document.querySelector(".matrix-form");
 
-day = mod(day);
-month = mod(month);
-year = mod(year);
+matrixForm.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-const bottom = mod(day + month + year);
-const center = mod(day + month + year + bottom);
+    // Removing the previous matrix from the page
+    const prevMatrix = document.querySelectorAll(".matrix__number").length;
+    prevMatrixDestroyer(prevMatrix);
 
-const dayMonth = mod(day + month);
-const monthYear = mod(month + year);
-const yearBottom = mod(year + bottom);
-const bottomDay = mod(bottom + day);
+    const {
+        elements: { birthday }
+    } = event.currentTarget;
 
-const centerDay = mod(center + day);
-const centerMonth = mod(center + month);
-const centerYear = mod(center + year);
-const centerBottom = mod(center + bottom);
+    let date = birthday.value.split(".");  
 
-const centerDayMonth = mod(center + dayMonth);
-const centerMonthYear = mod(center + monthYear);
-const centerYearBottom = mod(center + yearBottom);
-const centerBottomDay = mod(center + bottomDay);
+    const day = mod(parseInt(date[0]));
+    const month = mod(parseInt(date[1]));
+    const year = mod(parseInt(date[2]));
 
-const dayCenterDay = mod(day + centerDay);
-const monthCenterMonth = mod(month + centerMonth);
-const yearCenterYear = mod(year + centerYear);
-const bottomCenterBottom = mod(bottom + centerBottom);
+    // Counting new matrix
+    const matrixData = matrixCounter(day, month, year);
 
-const centerDayMonthDayMonth = mod(centerDayMonth + dayMonth);
-const centerMonthYearMonthYear = mod(centerMonthYear + monthYear);
-const centerYearBottomYearBottom = mod(centerYearBottom + yearBottom);
-const centerBottomDayBottomDay = mod(centerBottomDay + bottomDay);
+    // Adding matrix to the main page
+    markupCreator(matrixData);
+});
 
-const financeRelationship = mod(centerYear + centerBottom);
-const finance = mod(financeRelationship + centerYear);
-const relationship = mod(financeRelationship + centerBottom);
+function prevMatrixDestroyer(numberOfItems) {
+    for (let i = 0; i < numberOfItems; i += 1) {
+        const number = document.querySelector(".matrix__number");
+        number.remove();
+    }
+};
 
-const soulShit = mod(centerDay + center);
-const soulShit2 = mod(centerMonth + center);
+function matrixCounter(day, month, year) {
+    const array = [];
 
-matrixData.push(day, month, year, bottom, center,
-    dayMonth, monthYear, yearBottom, bottomDay,
-    centerDay, centerMonth, centerYear, centerBottom,
-    centerDayMonth, centerMonthYear, centerYearBottom, centerBottomDay,
-    dayCenterDay, monthCenterMonth, yearCenterYear, bottomCenterBottom,
-    centerDayMonthDayMonth, centerMonthYearMonthYear, centerYearBottomYearBottom, centerBottomDayBottomDay,
-    financeRelationship, finance, relationship,
-    soulShit, soulShit2
+    const bottom = mod(day + month + year);
+    const center = mod(day + month + year + bottom);
 
-);
+    const dayMonth = mod(day + month);
+    const monthYear = mod(month + year);
+    const yearBottom = mod(year + bottom);
+    const bottomDay = mod(bottom + day);
 
-// Adding shit to the main page
-const markup = matrixData.map(n => 
-    `<li class="matrix__number">
-        <p>${n}</p>
-    </li>`
-).join("");
+    const centerDay = mod(center + day);
+    const centerMonth = mod(center + month);
+    const centerYear = mod(center + year);
+    const centerBottom = mod(center + bottom);
 
-matrix.insertAdjacentHTML("beforeend", markup);
+    const centerDayMonth = mod(center + dayMonth);
+    const centerMonthYear = mod(center + monthYear);
+    const centerYearBottom = mod(center + yearBottom);
+    const centerBottomDay = mod(center + bottomDay);
+
+    const dayCenterDay = mod(day + centerDay);
+    const monthCenterMonth = mod(month + centerMonth);
+    const yearCenterYear = mod(year + centerYear);
+    const bottomCenterBottom = mod(bottom + centerBottom);
+
+    const centerDayMonthDayMonth = mod(centerDayMonth + dayMonth);
+    const centerMonthYearMonthYear = mod(centerMonthYear + monthYear);
+    const centerYearBottomYearBottom = mod(centerYearBottom + yearBottom);
+    const centerBottomDayBottomDay = mod(centerBottomDay + bottomDay);
+
+    const financeRelationship = mod(centerYear + centerBottom);
+    const finance = mod(financeRelationship + centerYear);
+    const relationship = mod(financeRelationship + centerBottom);
+
+    const soulSmth = mod(centerDay + center);
+    const soulSmth2 = mod(centerMonth + center);
+
+    array.push(day, month, year, bottom, center,
+        dayMonth, monthYear, yearBottom, bottomDay,
+        centerDay, centerMonth, centerYear, centerBottom,
+        centerDayMonth, centerMonthYear, centerYearBottom, centerBottomDay,
+        dayCenterDay, monthCenterMonth, yearCenterYear, bottomCenterBottom,
+        centerDayMonthDayMonth, centerMonthYearMonthYear, centerYearBottomYearBottom, centerBottomDayBottomDay,
+        financeRelationship, finance, relationship,
+        soulSmth, soulSmth2
+    );
+
+    return array;
+}
+
+function markupCreator(array) {
+    const markup = array.map(element => 
+        `<li class="matrix__number">
+            <p>${element}</p>
+        </li>`
+    ).join("");
+
+    matrix.insertAdjacentHTML("beforeend", markup); 
+}
